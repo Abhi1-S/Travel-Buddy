7d8452965d9d408fa9fe006d07639fc8eb4967df
S.Abhinay Reddy <sabhinayr@gmail.com>
2026-08-19 12:09:56 +0530

    Refactor Config class to use _get_secret method

--- config.py
+++ config.py
@@ -1,12 +1,36 @@
 import os
 import streamlit as st
 
+
 class Config:
+
     def __init__(self):
-        self.cohere_key = st.secrets.get("COHERE_API_KEY", os.getenv("COHERE_API_KEY"))
-        self.foursquare_key = st.secrets.get("FOURSQUARE_API_KEY", os.getenv("FOURSQUARE_API_KEY"))
-        self.google_cse_key = st.secrets.get("GOOGLE_CSE_API_KEY", os.getenv("GOOGLE_CSE_API_KEY"))
-        self.search_id = st.secrets.get("SEARCH_ENGINE_ID", os.getenv("SEARCH_ENGINE_ID"))
-        self.weather_key = st.secrets.get("WEATHERAPI_API_KEY", os.getenv("WEATHERAPI_API_KEY"))
+        self.cohere_key = self._get_secret(
+            "COHERE_API_KEY"
+        )
+
+        self.foursquare_key = self._get_secret(
+            "FOURSQUARE_API_KEY"
+        )
+
+        self.google_cse_key = self._get_secret(
+            "GOOGLE_CSE_API_KEY"
+        )
+
+        self.search_id = self._get_secret(
+            "SEARCH_ENGINE_ID"
+        )
+
+        self.weather_key = self._get_secret(
+            "WEATHERAPI_API_KEY"
+        )
+
+    @staticmethod
+    def _get_secret(name):
+        return st.secrets.get(
+            name,
+            os.getenv(name)
+        )
+
 
 config = Config()
